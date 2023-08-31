8e14f4bbf5c4090c2808f2dfc0a0fe244162266c
Joe Yang <yangxueminghao@163.com>
2023-08-31 15:54:31 +0800

    禁用自动保存

---
+++ demo/RulesEngineEditorServer/Migrations/20230830083558_InitMysql.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RulesEngineEditorServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMysql : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Workflows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    WorkflowName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Seq = table.Column<int>(type: "int", nullable: false),
+                    RuleExpressionType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workflows", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Rules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Seq = table.Column<int>(type: "int", nullable: false),
+                    RuleDataId = table.Column<int>(type: "int", nullable: true),
+                    WorkflowDataId = table.Column<int>(type: "int", nullable: true),
+                    RuleName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Properties = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Operator = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Enabled = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    RuleExpressionType = table.Column<int>(type: "int", nullable: false),
+                    Expression = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Actions = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SuccessEvent = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rules_Rules_RuleDataId",
+                        column: x => x.RuleDataId,
+                        principalTable: "Rules",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Rules_Workflows_WorkflowDataId",
+                        column: x => x.WorkflowDataId,
+                        principalTable: "Workflows",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ScopedParam",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Seq = table.Column<int>(type: "int", nullable: false),
+                    RuleDataId = table.Column<int>(type: "int", nullable: true),
+                    WorkflowDataId = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Expression = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScopedParam", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScopedParam_Rules_RuleDataId",
+                        column: x => x.RuleDataId,
+                        principalTable: "Rules",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ScopedParam_Workflows_WorkflowDataId",
+                        column: x => x.WorkflowDataId,
+                        principalTable: "Workflows",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rules_RuleDataId",
+                table: "Rules",
+                column: "RuleDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rules_WorkflowDataId",
+                table: "Rules",
+                column: "WorkflowDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScopedParam_RuleDataId",
+                table: "ScopedParam",
+                column: "RuleDataId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScopedParam_WorkflowDataId",
+                table: "ScopedParam",
+                column: "WorkflowDataId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScopedParam");
+
+            migrationBuilder.DropTable(
+                name: "Rules");
+
+            migrationBuilder.DropTable(
+                name: "Workflows");
+        }
+    }
+}
---
+++ demo/RulesEngineEditorServer/Migrations/20230830085414_InitMysql2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RulesEngineEditorServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMysql2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ErrorMessage",
+                table: "Rules",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ErrorMessage",
+                table: "Rules");
+        }
+    }
+}
